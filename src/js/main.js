62643d35c49391aa20a5f698ee5b91bc92472614
Mürsel Şen <46069077+murselsen@users.noreply.github.com>
2024-12-24 23:20:01 +0300

    Futuristic App - Code Files Ready

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  // Header
+  $('#page-header').load('partials/header.html');
+  // Hero
+  $('#hero').load('partials/hero.html');
+  // Organizations
+  $('#organizations').load('partials/organizations.html');
+  // Features
+  $('#features').load('partials/features.html');
+  // Downloads
+  $('#downloads').load('partials/downloads.html');
+  // Footer
+  $('#page-footer').load('partials/footer.html');
+});
